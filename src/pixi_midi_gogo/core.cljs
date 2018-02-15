8a9260af3283ebb4f9af24548efaf42db2dc7c96
oakes <zach@sekao.net>
2018-02-14 20:11:26 -0500

    Add clara and start playing with it

--- src/pixi_midi_gogo/core.cljs
+++ src/pixi_midi_gogo/core.cljs
@@ -1,3 +1,19 @@
-(ns pixi-midi-gogo.core)
+(ns pixi-midi-gogo.core
+  (:require [clara.rules :refer [insert fire-rules]])
+  (:require-macros [clara.rules :refer [defsession defrule]]))
 
-(js/console.log "HEY")
+(defrecord Person [name email])
+
+(defsession session 'pixi-midi-gogo.core)
+
+(enable-console-print!)
+
+(defrule alice
+  [Person (= "Alice" name) (= ?email email)]
+  =>
+  (js/console.log ?email))
+
+(-> session
+    (insert (->Person "Alice" "alice@sekao.net"))
+    (fire-rules))
+
