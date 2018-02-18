49ca225a1943eab25aad5c53360ffcb671fb85ed
oakes <zach@sekao.net>
2018-02-17 19:14:04 -0500

    Allow inserting during init and mount element

--- src/pixi_midi_gogo/core.clj
+++ src/pixi_midi_gogo/core.clj
@@ -5,19 +5,23 @@
 
 (defn add-rule [ns-sym name body]
   (->> (dsl/build-rule name body)
-       (swap! env/*compiler* assoc-in [:clara.macros/productions ns-sym name])))
+       (swap! env/*compiler* assoc-in [:clara.macros/productions ns-sym name]))
+  nil)
 
-(defn read-rules-for-ns [ns-sym rules]
-  (doseq [i (range (count rules))
+(defn read-rules-for-ns [[ns-sym rules]]
+  (remove nil?
+    (for [i (range (count rules))
           :let [{:keys [on do]} (get rules i)
                 sym (symbol (str "rule-" i))]]
-    (add-rule ns-sym sym (list on '=> do))))
+      (if on
+        (add-rule ns-sym sym (list on '=> do))
+        do))))
 
 (defmacro read-rules [& rules]
-  (let [ns->rules (group-by :in rules)]
-    (doseq [[ns-sym rules] ns->rules]
-      (read-rules-for-ns ns-sym rules))
-    (macros/sources-and-options->session-assembly-form
-      (for [ns-sym (keys ns->rules)]
-        (list 'quote ns-sym)))))
+  (let [ns->rules (group-by :in rules)
+        init-forms (mapcat read-rules-for-ns ns->rules)
+        session (macros/sources-and-options->session-assembly-form
+                  (for [ns-sym (keys ns->rules)]
+                    (list 'quote ns-sym)))]
+    `(-> ~session ~@init-forms)))
 
--- src/pixi_midi_gogo/core.cljs
+++ src/pixi_midi_gogo/core.cljs
@@ -1,24 +1,35 @@
 (ns pixi-midi-gogo.core
-  (:require [clara.rules :refer [query insert fire-rules]])
-  (:require-macros [clara.rules :refer [defsession defrule defquery]]
+  (:require [clara.rules :refer [insert fire-rules]]
+            [rum.core :as rum])
+  (:require-macros [clara.rules :refer [defsession defrule]]
                    [pixi-midi-gogo.core :refer [read-rules]]))
 
 (defrecord Person [name email])
+(defrecord Element [value parent])
 
-(def session
+(rum/defc empty-comp
+  [content]
+  content)
+
+(defrule elem
+  [?elem <- Element]
+  =>
+  (rum/mount
+    (empty-comp (:value ?elem))
+    (.querySelector js/document (:parent ?elem))))
+
+(fire-rules
   (read-rules
     {:in pixi-midi-gogo.core
      :on [?person <- Person]
      :do (js/console.log (pr-str ?person))}
     {:in pixi-midi-gogo.core
      :on [Person (= "Alice" name) (= ?email email)]
-     :do (js/console.log ?email)}))
-
-(defn init []
-  (-> session
-      (insert (->Person "Alice" "alice@sekao.net"))
-      (insert (->Person "Bob" "bob@sekao.net"))
-      (fire-rules)))
-
-(init)
+     :do (js/console.log ?email)}
+    {:in pixi-midi-gogo.core
+     :do (insert (->Person "Alice" "alice@sekao.net"))}
+    {:in pixi-midi-gogo.core
+     :do (insert (->Person "Bob" "bob@sekao.net"))}
+    {:in pixi-midi-gogo.core
+     :do (insert (->Element [:h1 "Hello, world!"] "#app"))}))
 
