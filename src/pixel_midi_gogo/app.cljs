503602a16ca69c9e33bb5cf304ef35d1d1a6b162
oakes <zach@sekao.net>
2018-02-21 16:06:29 -0500

    Move to external edn file

--- src/pixel_midi_gogo/app.cljs
+++ src/pixel_midi_gogo/app.cljs
@@ -4,47 +4,13 @@
             [pixel-midi-gogo.event :refer [Event]]
             [clara.rules :as rules]
             [clara.rules.accumulators :refer [all]])
-  (:require-macros [pixel-midi-gogo.core :refer [read-rules]]))
+  (:require-macros [pixel-midi-gogo.core :refer [init]]))
 
 (defrecord Person [name email])
 
 (defrecord ListItem [text])
 
-(read-rules
+(init
   [pixel-midi-gogo.core pixel-midi-gogo.view pixel-midi-gogo.event]
-  
-  (map->Def {:id :stuff, :value [:div "Hi"]})
-  (map->Def {:id :contact, :value (->Person "Alice" "alice@sekao.net")})
-  (map->Def {:id :contact, :value (->Person "Bob" "bob@sekao.net")})
-  
-  :select
-  [Person (= ?name name)]
-  :insert
-  (map->ListItem {:text (str "Hello, " ?name)})
-  
-  :select
-  [Person (= ?email email)]
-  [Def (= id :stuff) (= ?value value)]
-  :execute
-  (js/console.log ?email (pr-str ?value))
-  
-  :select
-  [Event (= ?id id) (= ?type type)]
-  :execute
-  (js/console.log "Event" (pr-str ?id) ?type)
-  
-  :select
-  [?items <- (all) :from [ListItem]]
-  :insert
-  (map->View
-    {:parent "#app"
-     :value [:div
-             [:input {:id :input
-                      :on-key-down true}]
-             [:button {:id :btn
-                       :on-click true}
-              "Click!"]
-             (into [:ul]
-               (for [item ?items]
-                 [:li (:text item)]))]}))
+  ["app.edn"])
 
--- src/pixel_midi_gogo/core.clj
+++ src/pixel_midi_gogo/core.clj
@@ -2,7 +2,10 @@
   (:require [clara.macros :as macros]
             [clara.rules :as rules]
             [cljs.env :as env]
-            [clara.rules.dsl :as dsl]))
+            [clara.rules.dsl :as dsl]
+            [clojure.java.io :as io]
+            [clojure.tools.reader :as r]
+            [clojure.tools.reader.reader-types :refer [indexing-push-back-reader]]))
 
 (def ^:const ns-sym 'pixel-midi-gogo.core)
 
@@ -24,22 +27,36 @@
                     (map #(list 'pixel-midi-gogo.core/insert %)
                       insert)))))
 
-(defmacro read-rules [nses & rules]
-  (let [init-forms (->> rules
-                        (take-while #(not (keyword? %)))
-                        (map #(list 'pixel-midi-gogo.core/insert %)))
-        rules (->> rules
-                   (drop-while #(not (keyword? %)))
-                   (partition-by keyword?)
-                   (partition 2)
-                   (reduce
-                     (fn [new-rules [header code]]
-                       (if (= (first header) :select)
-                         (conj new-rules {:select code})
-                         (if (seq new-rules)
-                           (update new-rules (dec (count new-rules))
-                             assoc (first header) code))))
-                     default-rules))
+(defn read-file [filename]
+  (let [reader (indexing-push-back-reader (slurp (io/reader filename)))]
+    (loop [forms []]
+      (if-let [form (r/read {:eof nil} reader)]
+        (recur (conj forms form))
+        forms))))
+
+(defn read-init-forms [forms]
+  (->> forms
+       (take-while #(not (keyword? %)))
+       (map #(list 'pixel-midi-gogo.core/insert %))))
+
+(defn read-rules [forms]
+  (->> forms
+       (drop-while #(not (keyword? %)))
+       (partition-by keyword?)
+       (partition 2)
+       (reduce
+         (fn [new-rules [header code]]
+           (if (= (first header) :select)
+             (conj new-rules {:select code})
+             (if (seq new-rules)
+               (update new-rules (dec (count new-rules))
+                 assoc (first header) code))))
+         default-rules)))
+
+(defmacro init [nses files]
+  (let [forms (mapv read-file files)
+        init-forms (vec (mapcat read-init-forms forms))
+        rules (vec (mapcat read-rules forms))
         _ (add-rules rules)
         session (macros/sources-and-options->session-assembly-form
                   (map #(list 'quote %) nses))]
