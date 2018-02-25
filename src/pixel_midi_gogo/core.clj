c007e99c0c185e9459d0487f5dc79779b097b1e1
oakes <zach@sekao.net>
2018-02-25 18:32:10 -0500

    Parse query forms with spec

--- src/pixel_midi_gogo/core.clj
+++ src/pixel_midi_gogo/core.clj
@@ -14,28 +14,43 @@
   (->> (dsl/build-rule name body)
        (swap! env/*compiler* assoc-in [:clara.macros/productions ns-sym name])))
 
-(defn transform-insert-form [[type value]]
-  (if (= type :record)
-    (let [{:keys [record args]} value]
-      (list
-        'pixel-midi-gogo.core/insert
-        (list
-          (symbol (str "map->" record))
-          (update
-            (->> args
-                 (map (juxt :key :val))
-                 (into {}))
-            :timestamp
-            #(or % '(.getTime (js/Date.)))))))
-    value))
+(defn transform-insert-form [{:keys [record args]}]
+  (list
+    'pixel-midi-gogo.core/insert
+    (list
+      (symbol (str "map->" record))
+      (update
+        (->> args
+             (map (juxt :key :val))
+             (into {}))
+        :timestamp
+        #(or % '(.getTime (js/Date.)))))))
+
+(defn transform-select-form [{:keys [binding record expressions args]}]
+  (let [query (vec (concat
+                     [record]
+                     expressions
+                     (map (fn [{:keys [key val]}]
+                            (list '= (symbol (name key)) val))
+                       args)))]
+    (if-let [{:keys [symbol arrow accumulator]} binding]
+      (if accumulator
+        [symbol arrow accumulator :from query]
+        (into [symbol arrow] query))
+      query)))
 
 (defn add-rules [rules]
   (swap! env/*compiler* assoc-in [:clara.macros/productions ns-sym] {})
   (doseq [i (range (count rules))
           :let [{:keys [select insert]} (get rules i)
                 sym (symbol (str "rule-" i))
-                right-side (map transform-insert-form (:forms insert))]]
-    (add-rule sym (concat (:forms select) ['=>]
+                left-side (mapv transform-select-form (:forms select))
+                right-side (map (fn [[type value]]
+                                  (if (= type :insert)
+                                    (transform-insert-form value)
+                                    value))
+                             (:forms insert))]]
+    (add-rule sym (concat left-side ['=>]
                     (if (empty? right-side) ['(do)] right-side)))))
 
 (defn read-file [filename]
@@ -45,28 +60,37 @@
         (recur (conj forms form))
         forms))))
 
+(s/def ::binding (s/cat
+                   :symbol symbol?
+                   :arrow #{'<-}
+                   :accumulator (s/? list?)))
 (s/def ::pair (s/cat
                 :key keyword?
                 :val any?))
-(s/def ::record-form (s/cat
+
+(s/def ::select-form (s/cat
+                       :binding (s/? ::binding)
+                       :record symbol?
+                       :expressions (s/* list?)
+                       :args (s/* ::pair)))
+(s/def ::insert-form (s/cat
                        :record symbol?
                        :args (s/* ::pair)))
 
-(s/def ::select-form vector?)
-(s/def ::insert-form (s/or
-                       :record ::record-form
-                       :execute list?))
 (s/def ::select-block (s/cat
                         :header #{:select}
-                        :forms (s/* ::select-form)))
+                        :forms (s/+ (s/spec ::select-form))))
 (s/def ::insert-block (s/cat
                         :header #{:insert}
-                        :forms (s/* ::insert-form)))
+                        :forms (s/+ (s/spec (s/or
+                                              :execute list?
+                                              :insert ::insert-form)))))
+
 (s/def ::rule (s/cat
                 :select ::select-block
-                :insert ::insert-block))
+                :insert (s/? ::insert-block)))
 (s/def ::file (s/cat
-                :init-forms (s/* ::insert-form)
+                :init-forms (s/* (s/spec ::insert-form))
                 :rules (s/* ::rule)))
 
 (defmacro init [nses files]
