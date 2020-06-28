c97d4139735655a24467846a0e6cca7ebd2d126d
oakes <zach@sekao.net>
2020-06-28 17:59:22 -0400

    Start 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.sekao/clarax "0.4.0"
+(defproject net.sekao/clarax "0.4.1-SNAPSHOT"
   :url "https://github.com/oakes/clarax"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
