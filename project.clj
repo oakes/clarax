fd3d22ad7f0cb7314cb2bbf21f589517299fb73c
oakes <zach@sekao.net>
2020-06-28 17:57:58 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.sekao/clarax "0.3.2-SNAPSHOT"
+(defproject net.sekao/clarax "0.4.0"
   :url "https://github.com/oakes/clarax"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
